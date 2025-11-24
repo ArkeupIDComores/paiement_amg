0eff226b1c938249fe2b87153768b4bf08752dbc
Iandraina <217048206+Iandraina-etech@users.noreply.github.com>
2025-11-24 09:49:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='openimis-be-amg-payement',
+    name='openimis-be-amg_payement',
     version='1.3.0',
     packages=find_packages(),
     include_package_data=True,
